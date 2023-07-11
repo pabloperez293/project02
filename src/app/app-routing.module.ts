2357394004f8ed848c1b652d4ff094ae3d3ce889
pabloperez293 <paperezpablo@gmail.com>
2023-07-11 14:26:45 -0300

    agregar/editar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComentariosComponent } from './components/list-comentarios/list-comentarios.component';
+import { AgregarEditarComentarioComponent } from './components/agregar-editar-comentario/agregar-editar-comentario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+// Se agregan objetos cuando la ruta este vacia
+  { path: "", component: ListComentariosComponent },
+  { path: "agregar", component: AgregarEditarComentarioComponent },
+  { path: "editar/:id", component: AgregarEditarComentarioComponent },
+  { path: "ver/:id", component: AgregarEditarComentarioComponent },
+  // Lo redirecciona al primer componente, esta ruta siempre tiene q estar en el final
+  { path: "**", redirectTo: "/", pathMatch:"full" }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
