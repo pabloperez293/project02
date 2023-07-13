71614b0f6afbfaa26c1b7d1db199947182414c9b
pabloperez293 <paperezpablo@gmail.com>
2023-07-13 17:16:54 -0300

    FrontEnd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListComentariosComponent } from './components/list-comentarios/list-comentarios.component';
 import { AgregarEditarComentarioComponent } from './components/agregar-editar-comentario/agregar-editar-comentario.component';
+import { VerComentariosComponent } from './components/ver-comentarios/ver-comentarios.component';
 
 const routes: Routes = [
 // Se agregan objetos cuando la ruta este vacia
   { path: "", component: ListComentariosComponent },
   { path: "agregar", component: AgregarEditarComentarioComponent },
   { path: "editar/:id", component: AgregarEditarComentarioComponent },
-  { path: "ver/:id", component: AgregarEditarComentarioComponent },
+  { path: "ver/:id", component: VerComentariosComponent },
   // Lo redirecciona al primer componente, esta ruta siempre tiene q estar en el final
   { path: "**", redirectTo: "/", pathMatch:"full" }
 
